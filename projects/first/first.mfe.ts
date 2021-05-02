4972f14661b3431e64c7dce18423f2f039cadd0e
Tai Tran <tai.phuc.tran@gmail.com>
2021-05-02 13:37:53 +0700

    Add Ag-grid

--- projects/first/first.mfe.ts
+++ projects/first/first.mfe.ts
@@ -1,5 +1,6 @@
 import {SharedMfe, SharedMfeModule} from '@angular-mfe/shared';
 import {Component, NgModule} from '@angular/core';
+import { AgGridModule } from 'ag-grid-angular';
 
 export const id = 'FirstMfeModule';
 
@@ -7,21 +8,38 @@ export const id = 'FirstMfeModule';
   selector: 'first-mfe',
   template: `
     <p>First Angular Micro Frontend</p> 
+    <ag-grid-angular
+    style="width: 500px; height: 150px;"
+    class="ag-theme-alpine"
+    [rowData]="rowData"
+    [columnDefs]="columnDefs">
+</ag-grid-angular>
     <p>
       <shared-component></shared-component>
     </p>
   `
 })
 export class EntryComponent {
-  constructor(private sharedMfe: SharedMfe) {
-    sharedMfe.registerMfe('MFE-ONE', EntryComponent);
-  }
+    columnDefs = [
+        { field: 'make' },
+        { field: 'model' },
+        { field: 'price' }
+    ];
+
+    rowData = [
+        { make: 'Toyota', model: 'Celica', price: 35000 },
+        { make: 'Ford', model: 'Mondeo', price: 32000 },
+        { make: 'Porsche', model: 'Boxter', price: 72000 }
+    ];
+    constructor(private sharedMfe: SharedMfe) {
+        sharedMfe.registerMfe('MFE-ONE', EntryComponent);
+    }
 }
 
 @NgModule({
   id,
   declarations: [EntryComponent],
-  imports: [SharedMfeModule]
+  imports: [SharedMfeModule,AgGridModule]
 })
 export class FirstMfeModule {
 }
